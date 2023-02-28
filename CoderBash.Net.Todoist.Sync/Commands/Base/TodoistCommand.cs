e259ffbf7fe789f46e3bcb57a8a978df3479de6b
nicodemar <nicolas@demarbit.com>
2023-02-28 11:30:31 +0100

    Adding first command for structure

---
+++ CoderBash.Net.Todoist.Sync/Commands/Base/TodoistCommand.cs
@@ -0,0 +1,31 @@
+﻿using CoderBash.Net.Todoist.Common.Extensions;
+using Newtonsoft.Json;
+
+namespace CoderBash.Net.Todoist.Sync.Commands.Base
+{
+    public abstract class TodoistCommand
+    {
+        private readonly string _commandUniqueId = Guid.NewGuid().ToString();
+        public string CommandUniqueId => _commandUniqueId;
+
+        protected string? _temporaryId;
+        public string CommandTemporaryId => _temporaryId ?? "";
+
+        protected abstract string CommandType { get; }
+        protected abstract Dictionary<string, object> GetCommandArgs();
+
+        public string GenerateCommand()
+        {
+            var commandObject = new Dictionary<string, object>()
+            {
+                ["type"] = CommandType,
+                ["uuid"] = _commandUniqueId,
+                ["args"] = GetCommandArgs()
+            };
+
+            commandObject.TryAddValue("temp_id", _temporaryId);
+
+            return JsonConvert.SerializeObject(commandObject);
+        }
+    }
+}
---
+++ CoderBash.Net.Todoist.Sync/Commands/Projects/AddProjectCommand.cs
@@ -0,0 +1,39 @@
+﻿using CoderBash.Net.Todoist.Common.Enums;
+using CoderBash.Net.Todoist.Common.Extensions;
+using CoderBash.Net.Todoist.Sync.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderBash.Net.Todoist.Sync.Commands.Projects
+{
+    public class AddProjectCommand : TodoistCommand
+    {
+        public string Name { get; set; } = null!;
+        public TodoistColors? Color { get; set; }
+        public string? ParentId { get; set; }
+        public int? ChildOrder { get; set; }
+        public bool? IsFavorite { get; set; }
+        public TodoistViewStyles? ViewStyle { get; set; }
+
+        protected override string CommandType => "project_add";
+
+        protected override Dictionary<string, object> GetCommandArgs()
+        {
+            var args = new Dictionary<string, object>()
+            {
+                ["name"] = Name
+            };
+
+            args.TryAddValue("color", TodoistColorsUtils.GetColorKey(Color));
+            args.TryAddValue("parent_id", ParentId);
+            args.TryAddValue("child_order", ChildOrder);
+            args.TryAddValue("is_favorite", IsFavorite);
+            args.TryAddValue("view_style", TodoistViewStylesUtils.GetViewStyleKey(ViewStyle));
+
+            return args;
+        }
+    }
+}
